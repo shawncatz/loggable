dc957e63cf5d9dfeefb778785d0bc9958e234c67
Shawn Catanzarite <me@shawncatz.com>
2014-09-18 18:44:37 -0700

    get config and define method working

--- lib/loggable.rb
+++ lib/loggable.rb
@@ -1,16 +1,15 @@
+require 'yell'
 module Loggable
   class << self
-
-    private
-
     def setup
       @loggable ||= begin
         loggable = YAML.load_file("config/loggable.yml")
+        puts "loggable.yml: #{loggable.inspect}"
         loggable['loggers'] = []
         loggable['logs'].each do |name, logger|
+          puts "#{name}: #{logger.inspect}"
           loggable['loggers'] << name
-          loggable[name] = Yall.new do |l|
-            l.level = logger['level'] if logger['level']
+          loggable[name] = Yell.new(name: name) do |l|
             l.adapter :file, logger['file'], level: logger['level']
           end
         end
@@ -18,7 +17,7 @@ module Loggable
       end
       @defined ||= begin
         @loggable['loggers'].each do |name|
-          define_method(name) do
+          define_singleton_method(name) do
             if @loggable['aliases'][name]
               @loggable['aliases'][name]
             else
