493846df76bc5e8040050a3f8d4a3fc12a813115
Shawn Catanzarite <me@shawncatz.com>
2024-01-04 19:05:57 -0800

    support stdout

--- lib/loggable.rb
+++ lib/loggable.rb
@@ -1,11 +1,11 @@
-require 'loggable/version'
-require 'loggable/engine'
-require 'loggable/integration'
-require 'loggable/message'
-require 'loggable/logger'
+require "loggable/version"
+require "loggable/engine"
+require "loggable/integration"
+require "loggable/message"
+require "loggable/logger"
 
-require 'yell'
-require 'active_support/all'
+require "yell"
+require "active_support/all"
 
 module Loggable
   class << self
@@ -14,29 +14,35 @@ module Loggable
     end
 
     def plugins
-      config['plugins']||[]
+      config["plugins"] || []
     end
 
     def setup
       @loggable ||= begin
-        loggable = YAML.load_file("config/loggable.yml")
-        loggable['loggers'] = []
-        loggable['logs'].each do |name, logger|
-          loggable['loggers'] << name
-          define_singleton_method(name) do
-            @loggable[name]
+          loggable = YAML.load_file("config/loggable.yml")
+          loggable["loggers"] = []
+          loggable["logs"].each do |name, logger|
+            loggable["loggers"] << name
+            define_singleton_method(name) do
+              @loggable[name]
+            end
+            if logger["file"].present?
+              loggable[name] = Yell.new(name: name) do |l|
+                l.adapter :file, logger["file"], level: logger["level"]
+              end
+            else
+              loggable[name] = Yell.new(name: name) do |l|
+                l.adapter :stdout, level: logger["level"]
+              end
+            end
           end
-          loggable[name] = Yell.new(name: name) do |l|
-            l.adapter :file, logger['file'], level: logger['level']
+          loggable["aliases"].each do |name, to|
+            define_singleton_method(name) do
+              @loggable[to]
+            end
           end
+          loggable
         end
-        loggable['aliases'].each do |name, to|
-          define_singleton_method(name) do
-            @loggable[to]
-          end
-        end
-        loggable
-      end
     end
   end
 end
